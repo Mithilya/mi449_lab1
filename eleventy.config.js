5c47ef01aff37de0a4c65e8a5bd7f25f520587c9
Mithilya Srinivasan <sriniv92@msu.edu>
2024-01-22 23:45:33 -0500

    updated

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css/fonts");
     eleventyConfig.addPassthroughCopy("**/*.png");
+    eleventyConfig.addPassthroughCopy("src/img");
 };
